37b519f9853fbbdd22599af482ce3cdda729f6a6
Vianney Barrientos <vianneybr02@outlook.com>
2025-07-07 13:47:13 -0600

    Controlador

--- PL/Controllers/UsuarioController.cs
+++ PL/Controllers/UsuarioController.cs
@@ -8,9 +8,49 @@ namespace PL.Controllers
 {
     public class UsuarioController : Controller
     {
-        // GET: Usuario
-        public ActionResult Index()
+        [HttpGet]// GET: Usuario
+        public ActionResult GetAll()
         {
+            ML.Usuario usuario = new ML.Usuario();
+            ML.Result result = BL.Usuario.GetAll();
+
+            return View(usuario);
+        }
+        [HttpGet]
+        public ActionResult Delete(int IdUsuario)
+        {
+            ML.Usuario usuario = new ML.Usuario();
+
+            ML.Result result = BL.Usuario.Delete(IdUsuario);
+
+            return RedirectToAction("GetAll");
+        }
+
+        [HttpGet]
+        public ActionResult Formulario(int IdUSuario) //GetById
+        {
+            ML.Usuario usuario = new ML.Usuario();
+            
+
+            if(IdUSuario > 0)
+            {
+                ML.Result result = BL.Usuario.GetById(IdUSuario);
+
+            }
+            return View();
+        }
+
+       [HttpPost]
+       public ActionResult Formulario (ML.Usuario usuario)
+        {
+            if(usuario.IdUsuario == 0)
+            {
+                ML.Result result = BL.Usuario.Add(usuario);
+            }
+            else
+            {
+                ML.Result result = BL.Usuario.Update(usuario);k
+            }
             return View();
         }
     }
